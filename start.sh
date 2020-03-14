db094604976ed7b34956cac48d418dd9e0fb1724
antimodes201 <bob.krause201@gmail.com>
2020-03-14 19:08:13 -0400

    change run dir for mod support

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 # Start script for Ark called from docker
 
 #check if master
@@ -9,4 +9,5 @@ fi
 
 # Launch Server
 # Variables pulled from Docker environment
+cd /ark/ShooterGame/Binaries/Linux
 /ark/ShooterGame/Binaries/Linux/ShooterGameServer ${MAP}?listen?Port=${GAME_PORT}?QueryPort=${QUERY_PORT}?RCONPort=${RCON_PORT}?SessionName=\"${INSTANCE_NAME}\"${ADDITIONAL_OPTS} ${ADDITIONAL_ARGS} ${BattleEye} -nosteamclient -server -log -servergamelog
